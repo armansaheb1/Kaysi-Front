b547a221c1df213b89b961c28d706354bf51d7b6
armansaheb1 <armansaheb@gmail.com>
2024-04-03 10:14:44 +0000

    Update

--- src/views/pages/Index.vue
+++ src/views/pages/Index.vue
@@ -37,52 +37,60 @@
       </h3>
       <br>
 
-      <div style="" class="card third">
-        <div class="card-header"><img src="/logo.png" style="border-radius: 50%;width: 60%; margin: auto" alt=""></div>
-        <div class="card-body" style="text-align: justify; direction: rtl;">
-          <h3>
-            کی سی چیست ؟
-          </h3>
-          <hr>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
+      <div style="padding: 5%">
+        <div style="" class=" card third">
+          <div class="card-header"><img src="/logo.png" style="border-radius: 50%;width: 60%; margin: auto" alt="">
+          </div>
+          <div class="card-body" style="text-align: justify; direction: rtl;">
+            <h3>
+              کی سی چیست ؟
+            </h3>
+            <hr>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </p>
+          </div>
         </div>
-      </div>
-      <div style="" class="card third">
-        <div class="card-header"><img src="/logo.png" style="border-radius: 50%;width: 60%; margin: auto" alt=""></div>
-        <div class="card-body" style="text-align: justify; direction: rtl;">
-          <h3>
-            کی سی چیست ؟
-          </h3>
-          <hr>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
+        <div style="" class="card third">
+          <div class="card-header"><img src="/logo.png" style="border-radius: 50%;width: 60%; margin: auto" alt="">
+          </div>
+          <div class="card-body" style="text-align: justify; direction: rtl;">
+            <h3>
+              کی سی چیست ؟
+            </h3>
+            <hr>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </p>
+          </div>
         </div>
-      </div>
-      <div style="" class="card third">
-        <div class="card-header"><img src="/logo.png" style="border-radius: 50%;width: 60%; margin: auto" alt=""></div>
-        <div class="card-body" style="text-align: justify; direction: rtl;">
-          <h3>
-            کی سی چیست ؟
-          </h3>
-          <hr>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
+        <div style="" class="card third">
+          <div class="card-header"><img src="/logo.png" style="border-radius: 50%;width: 60%; margin: auto" alt="">
+          </div>
+          <div class="card-body" style="text-align: justify; direction: rtl;">
+            <h3>
+              کی سی چیست ؟
+            </h3>
+            <hr>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </p>
+          </div>
         </div>
       </div>
     </div>
